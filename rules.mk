5cb69453e7d9f096c4b3e431b11a5526439dce9f
Bryan Flynt <72622421+BryanFlynt@users.noreply.github.com>
2022-10-27 12:41:00 -0600

    Fixed rules.mk dependencies

--- rules.mk
+++ rules.mk
@@ -1,17 +1,19 @@
 
 .PHONY: clean cleanmem cleanall
 
-all : unpack_only compilers libraries
+all : unpack_only system_only compilers libraries mpi_compilers libraries_w_mpi
 
 unpack_only : cmake paraview
 
+system_only : ninja cgal
+
 compilers : gcc llvm julia
 
-libraries : ninja cuda hwloc ucx libevent boost openblas blis cgal
+libraries : cuda hwloc ucx libevent boost openblas blis
 
 mpi_compilers : openmpi
 
-libraries_w_mpi : boost-mpi gptl hdf5 netcdf
+libraries_w_mpi : boost-mpi #gptl hdf5 netcdf
 
 clean :
 	rm -rf log
@@ -34,7 +36,7 @@ cleanall : cleanmem
 # CMake
 # -----------------------------------------------
 
-cmake : cmake-3.20.2 cmake-3.23.2
+cmake : cmake-3.23.2 #cmake-3.20.2
 
 cmake-3.20.2 : ${MODULE_DIR}/base/cmake/3.20.2.lua
 
@@ -56,6 +58,34 @@ paraview-5.10.1 : ${MODULE_DIR}/base/paraview/5.10.1.lua
 
 ${MODULE_DIR}/base/paraview/5.10.1.lua:
 	${SRC_DIR}/build.sh paraview 5.10.1
+	
+#
+# **********************************************************
+#                   System Compiler (ONLY)
+# **********************************************************
+#
+
+# -----------------------------------------------
+# Ninja
+# -----------------------------------------------
+
+ninja : ninja-1.11.0
+
+ninja-1.11.0 : cmake ${MODULE_DIR}/base/ninja/1.11.0.lua
+
+${MODULE_DIR}/base/ninja/1.11.0.lua:
+	${SRC_DIR}/build.sh ninja 1.11.0
+
+# -----------------------------------------------
+# CGAL
+# -----------------------------------------------
+
+cgal : cgal-5.4.1
+
+cgal-5.4.1 : cmake ${MODULE_DIR}/base/cgal/5.4.1.lua
+
+${MODULE_DIR}/base/cgal/5.4.1.lua:
+	${SRC_DIR}/build.sh cgal 5.4.1
 
 #
 # **********************************************************
@@ -84,19 +114,19 @@ ${MODULE_DIR}/base/gcc/11.3.0.lua:
 
 llvm : llvm-14.0.4 llvm-dev
 
-llvm-6.0.0 : ${MODULE_DIR}/base/llvm/6.0.0.lua
+llvm-6.0.0 : cmake ${MODULE_DIR}/base/llvm/6.0.0.lua
 
-llvm-14.0.4 : ${MODULE_DIR}/base/llvm/14.0.4.lua
+llvm-14.0.4 : cmake ${MODULE_DIR}/base/llvm/14.0.4.lua
 
-llvm-dev : ${MODULE_DIR}/base/llvm/dev.lua
+llvm-dev : cmake ${MODULE_DIR}/base/llvm/dev.lua
 
-${MODULE_DIR}/base/llvm/6.0.0.lua: cmake
+${MODULE_DIR}/base/llvm/6.0.0.lua:
 	${SRC_DIR}/build.sh llvm 6.0.0
 
-${MODULE_DIR}/base/llvm/14.0.4.lua: cmake
+${MODULE_DIR}/base/llvm/14.0.4.lua:
 	${SRC_DIR}/build.sh llvm 14.0.4
 
-${MODULE_DIR}/base/llvm/dev.lua: cmake
+${MODULE_DIR}/base/llvm/dev.lua:
 	${SRC_DIR}/build.sh llvm dev
 
 # -----------------------------------------------
@@ -122,21 +152,10 @@ ${MODULE_DIR}/base/julia/1.8.1.lua:
 
 cuda : cuda-11.7.1
 
-cuda-11.7.1 : ${MODULE_DIR}/base/cuda/11.7.1.lua
+cuda-11.7.1 : cmake ${MODULE_DIR}/base/cuda/11.7.1.lua
 
-${MODULE_DIR}/base/cuda/11.7.1.lua : cmake
+${MODULE_DIR}/base/cuda/11.7.1.lua:
 	${SRC_DIR}/build.sh cuda 11.7.1
-
-# -----------------------------------------------
-# Ninja
-# -----------------------------------------------
-
-ninja : ninja-1.11.0
-
-ninja-1.11.0 : ${MODULE_DIR}/base/ninja/1.11.0.lua
-
-${MODULE_DIR}/base/ninja/1.11.0.lua : cmake
-	${SRC_DIR}/build.sh ninja 1.11.0
 
 # -----------------------------------------------
 # ISL (Download Only)
@@ -184,14 +203,14 @@ boost-gcc : boost-1.79.0-gcc-11.3.0
 
 boost-llvm : boost-1.79.0-llvm-14.0.4
 
-boost-1.79.0-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/boost/1.79.0.lua
+boost-1.79.0-gcc-11.3.0 : gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/boost/1.79.0.lua
 
-boost-1.79.0-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/boost/1.79.0.lua
+boost-1.79.0-llvm-14.0.4 : llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/boost/1.79.0.lua
 
-${MODULE_DIR}/compiler/gcc/11.3.0/boost/1.79.0.lua : gcc-11.3.0
+${MODULE_DIR}/compiler/gcc/11.3.0/boost/1.79.0.lua:
 	${SRC_DIR}/build.sh boost 1.79.0 gcc 11.3.0
 
-${MODULE_DIR}/compiler/llvm/14.0.4/boost/1.79.0.lua : llvm-14.0.4
+${MODULE_DIR}/compiler/llvm/14.0.4/boost/1.79.0.lua:
 	${SRC_DIR}/build.sh boost 1.79.0 llvm 14.0.4
 
 # -----------------------------------------------
@@ -204,14 +223,14 @@ hwloc-2.7.1-gcc : hwloc-2.7.1-gcc-11.3.0
 
 hwloc-2.7.1-llvm : hwloc-2.7.1-llvm-14.0.4
 
-hwloc-2.7.1-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/hwloc/2.7.1.lua
+hwloc-2.7.1-gcc-11.3.0 : gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/hwloc/2.7.1.lua
 
-hwloc-2.7.1-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/hwloc/2.7.1.lua
+hwloc-2.7.1-llvm-14.0.4 : llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/hwloc/2.7.1.lua
 
-${MODULE_DIR}/compiler/gcc/11.3.0/hwloc/2.7.1.lua : gcc-11.3.0
+${MODULE_DIR}/compiler/gcc/11.3.0/hwloc/2.7.1.lua:
 	${SRC_DIR}/build.sh hwloc 2.7.1 gcc 11.3.0
 
-${MODULE_DIR}/compiler/llvm/14.0.4/hwloc/2.7.1.lua : llvm-14.0.4
+${MODULE_DIR}/compiler/llvm/14.0.4/hwloc/2.7.1.lua:
 	${SRC_DIR}/build.sh hwloc 2.7.1 llvm 14.0.4
 
 # -----------------------------------------------
@@ -224,14 +243,14 @@ ucx-1.12.1-gcc : ucx-1.12.1-gcc-11.3.0
 
 ucx-1.12.1-llvm : ucx-1.12.1-llvm-14.0.4
 
-ucx-1.12.1-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/ucx/1.12.1.lua
+ucx-1.12.1-gcc-11.3.0 : gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/ucx/1.12.1.lua
 
-ucx-1.12.1-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/ucx/1.12.1.lua
+ucx-1.12.1-llvm-14.0.4 : llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/ucx/1.12.1.lua
 
-${MODULE_DIR}/compiler/gcc/11.3.0/ucx/1.12.1.lua : gcc-11.3.0
+${MODULE_DIR}/compiler/gcc/11.3.0/ucx/1.12.1.lua:
 	${SRC_DIR}/build.sh ucx 1.12.1 gcc 11.3.0
 
-${MODULE_DIR}/compiler/llvm/14.0.4/ucx/1.12.1.lua : llvm-14.0.4
+${MODULE_DIR}/compiler/llvm/14.0.4/ucx/1.12.1.lua:
 	${SRC_DIR}/build.sh ucx 1.12.1 llvm 14.0.4
 
 # -----------------------------------------------
@@ -244,25 +263,25 @@ libevent-2.1.12-gcc : libevent-2.1.12-gcc-11.3.0
 
 libevent-2.1.12-llvm : libevent-2.1.12-llvm-14.0.4
 
-libevent-2.1.12-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/libevent/2.1.12.lua
+libevent-2.1.12-gcc-11.3.0 : gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/libevent/2.1.12.lua
 
-libevent-2.1.12-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/libevent/2.1.12.lua
+libevent-2.1.12-llvm-14.0.4 : llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/libevent/2.1.12.lua
 
-${MODULE_DIR}/compiler/gcc/11.3.0/libevent/2.1.12.lua : gcc-11.3.0
+${MODULE_DIR}/compiler/gcc/11.3.0/libevent/2.1.12.lua:
 	${SRC_DIR}/build.sh libevent 2.1.12 gcc 11.3.0
 
-${MODULE_DIR}/compiler/llvm/14.0.4/libevent/2.1.12.lua : llvm-14.0.4
+${MODULE_DIR}/compiler/llvm/14.0.4/libevent/2.1.12.lua:
 	${SRC_DIR}/build.sh libevent 2.1.12 llvm 14.0.4
 
 # -----------------------------------------------
 # OpenBLAS
 # -----------------------------------------------
 
-openblas : openblas-0.3.20-gcc-11.3.0
+openblas : openblas-0.3.20-gcc-11.3.0 #openblas-0.3.20-llvm-14.0.4
 
-openblas-0.3.20-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/openblas/0.3.20.lua
+openblas-0.3.20-gcc-11.3.0 : gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/openblas/0.3.20.lua
 
-openblas-0.3.20-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/openblas/0.3.20.lua
+openblas-0.3.20-llvm-14.0.4 : llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/openblas/0.3.20.lua
 
 ${MODULE_DIR}/compiler/gcc/11.3.0/openblas/0.3.20.lua:
 	${SRC_DIR}/build.sh openblas 0.3.20 gcc 11.3.0
@@ -276,9 +295,9 @@ ${MODULE_DIR}/compiler/llvm/14.0.4/openblas/0.3.20.lua:
 
 blis : blis-0.9.0-gcc-11.3.0 blis-0.9.0-llvm-14.0.4
 
-blis-0.9.0-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/blis/0.9.0.lua
+blis-0.9.0-gcc-11.3.0 : gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/blis/0.9.0.lua
 
-blis-0.9.0-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/blis/0.9.0.lua
+blis-0.9.0-llvm-14.0.4 : llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/blis/0.9.0.lua
 
 ${MODULE_DIR}/compiler/gcc/11.3.0/blis/0.9.0.lua:
 	${SRC_DIR}/build.sh blis 0.9.0 gcc 11.3.0
@@ -287,26 +306,20 @@ ${MODULE_DIR}/compiler/llvm/14.0.4/blis/0.9.0.lua:
 	${SRC_DIR}/build.sh blis 0.9.0 llvm 14.0.4
 
 # -----------------------------------------------
-# CGAL
-# -----------------------------------------------
-
-cgal : cgal-5.4.1
-
-cgal-5.4.1 : ${MODULE_DIR}/base/cgal/5.4.1.lua
-
-${MODULE_DIR}/base/cgal/5.4.1.lua:
-	${SRC_DIR}/build.sh cgal 5.4.1
-
-# -----------------------------------------------
 # OCCA
 # -----------------------------------------------
 
-occa : occa-1.4.0-gcc-11.3.0
+occa : occa-1.4.0-gcc-11.3.0 occa-1.4.0-llvm-14.0.4
 
-occa-1.4.0-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/occa/1.4.0.lua
+occa-1.4.0-gcc-11.3.0 : cmake gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/occa/1.4.0.lua
+
+occa-1.4.0-llvm-14.0.4 : cmake llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/occa/1.4.0.lua
 
 ${MODULE_DIR}/compiler/gcc/11.3.0/occa/1.4.0.lua:
 	${SRC_DIR}/build.sh occa 1.4.0 gcc 11.3.0
+
+${MODULE_DIR}/compiler/llvm/14.0.4/occa/1.4.0.lua:
+	${SRC_DIR}/build.sh occa 1.4.0 llvm 14.0.4
 
 #
 # **********************************************************
@@ -324,9 +337,9 @@ openmpi-4.1.4-gcc : openmpi-4.1.4-gcc-11.3.0
 
 openmpi-4.1.4-llvm : openmpi-4.1.4-llvm-14.0.4
 
-openmpi-4.1.4-gcc-11.3.0 : ${MODULE_DIR}/compiler/gcc/11.3.0/openmpi/4.1.4.lua
+openmpi-4.1.4-gcc-11.3.0 : hwloc-2.7.1-gcc-11.3.0 ucx-1.12.1-gcc-11.3.0 libevent-2.1.12-gcc-11.3.0 ${MODULE_DIR}/compiler/gcc/11.3.0/openmpi/4.1.4.lua
 
-openmpi-4.1.4-llvm-14.0.4 : ${MODULE_DIR}/compiler/llvm/14.0.4/openmpi/4.1.4.lua
+openmpi-4.1.4-llvm-14.0.4 : hwloc-2.7.1-llvm-14.0.4 ucx-1.12.1-llvm-14.0.4 libevent-2.1.12-llvm-14.0.4 ${MODULE_DIR}/compiler/llvm/14.0.4/openmpi/4.1.4.lua
 
 ${MODULE_DIR}/compiler/gcc/11.3.0/openmpi/4.1.4.lua:
 	${SRC_DIR}/build.sh openmpi 4.1.4 gcc 11.3.0
@@ -344,11 +357,18 @@ ${MODULE_DIR}/compiler/llvm/14.0.4/openmpi/4.1.4.lua:
 # Boost
 # -----------------------------------------------
 
-boost-mpi : boost-mpi-gcc
+boost-mpi : boost-mpi-gcc boost-mpi-llvm
 
 boost-mpi-gcc : boost-1.79.0-openmpi-4.1.4-gcc-11.3.0
 
-boost-1.79.0-openmpi-4.1.4-gcc-11.3.0 : ${MODULE_DIR}/mpi/openmpi/4.1.4/gcc/11.3.0/boost/1.79.0.lua
+boost-mpi-llvm : boost-1.79.0-openmpi-4.1.4-llvm-14.0.4
+
+boost-1.79.0-openmpi-4.1.4-gcc-11.3.0 : openmpi-4.1.4-gcc-11.3.0 ${MODULE_DIR}/mpi/openmpi/4.1.4/gcc/11.3.0/boost/1.79.0.lua
+
+boost-1.79.0-openmpi-4.1.4-llvm-14.0.4 : openmpi-4.1.4-llvm-14.0.4 ${MODULE_DIR}/mpi/openmpi/4.1.4/llvm/14.0.4/boost/1.79.0.lua
 
 ${MODULE_DIR}/mpi/openmpi/4.1.4/gcc/11.3.0/boost/1.79.0.lua:
 	${SRC_DIR}/build.sh boost 1.79.0 gcc 11.3.0 openmpi 4.1.4
+	
+${MODULE_DIR}/mpi/openmpi/4.1.4/llvm/14.0.4/boost/1.79.0.lua:
+	${SRC_DIR}/build.sh boost 1.79.0 llvm 14.0.4 openmpi 4.1.4
